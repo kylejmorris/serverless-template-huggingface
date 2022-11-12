e909a28b9eb474b26ad16c79c1af71ea45f5ebe9
Kyle Morris <kylemorriscs@gmail.com>
2022-11-12 11:59:10 -0800

    Update test_env.py

--- src/test_env.py
+++ src/test_env.py
@@ -3,4 +3,5 @@ if __name__ == '__main__':
 	# testing build args can be set via banana UI 
 	if os.getenv("HF_AUTH_TOKEN") is not None:
 		print("found auth token test passed! exiting build!")
+		print(os.getenv("HF_AUTH_TOKEN"))
 		import paoiasjdf # force an error to throw to bail out early
